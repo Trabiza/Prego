0d2cc7be61800edb4d5cadd25af885fa93613be1
Mahmoudheshmat159 <Mahmoudheshmat159@gmail.com>
2019-08-19 11:16:15 +0200

    Initial Commit

---
+++ Prego/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prego
+//
+//  Created by owner on 8/19/19.
+//  Copyright © 2019 Y2M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
